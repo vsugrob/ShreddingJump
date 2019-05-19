92e25e1738641360ac5e207d123fae975c05e790
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-19 19:11:15 +0300

    Implement rotating killer obstacle.

--- Assets/Code/MathHelper.cs
+++ Assets/Code/MathHelper.cs
@@ -1,4 +1,41 @@
-﻿public static class MathHelper {
+﻿using System;
+using UnityEngine;
+
+public static class MathHelper {
+	public const float Pi2 = Mathf.PI * 2;
+	public static void Swap <T> ( ref T v1, ref T v2 ) {
+		var tmp = v1;
+		v1 = v2;
+		v2 = tmp;
+	}
+
+	public static bool SortMinMax <T> ( ref T vMin, ref T vMax )
+		where T : IComparable <T>
+	{
+		if ( vMin.CompareTo ( vMax ) > 0 ) {
+			Swap ( ref vMin, ref vMax );
+			return	true;
+		}
+
+		return	false;
+	}
+
+	public static float ToNormAngle ( float angle ) {
+		angle = angle % Pi2;
+		if ( angle < 0 )
+			angle += Pi2;
+
+		return	angle;
+	}
+
+	public static float ToNormAngleDeg ( float angle ) {
+		angle = angle % 360;
+		if ( angle < 0 )
+			angle += 360;
+
+		return	angle;
+	}
+
 	public static bool StepTowards ( ref float value, float target, float step ) {
 		var diff = target - value;
 		bool stepIsExcessive = diff > 0 ? diff <= step : diff >= step;
@@ -8,5 +45,33 @@
 			value += step;
 
 		return	stepIsExcessive;
+	}
+
+	public static bool StepTowardsAngleDeg ( ref float angle, float targetAngle, float step ) {
+		angle = ToNormAngleDeg ( angle );
+		targetAngle = ToNormAngleDeg ( targetAngle );
+		return	StepTowardsAngleDegNorm ( ref angle, targetAngle, step );
+	}
+
+	public static bool StepTowardsAngleDegNorm ( ref float angleNorm, float targetAngleNorm, float step ) {
+		var diff = targetAngleNorm - angleNorm;
+		bool stepIsExcessive = diff > 0 ? diff <= step : diff >= step;
+		if ( stepIsExcessive )
+			angleNorm = targetAngleNorm;
+		else
+			angleNorm += step;
+
+		return	stepIsExcessive;
+	}
+
+	public static float ArcBetweenDegNorm ( float startAngleNorm, float endAngleNorm, bool positiveStep ) {
+		float diff = endAngleNorm - startAngleNorm;
+		if ( diff >= 0 ) {
+			if ( !positiveStep )
+				diff -= 360;
+		} else if ( positiveStep )
+			diff += 360;
+
+		return	diff;
 	}
 }
---
+++ Assets/Code/ObjectRotator.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+public class ObjectRotator : MonoBehaviour {
+	[SerializeField]
+	private float _angularSpeed = 135;
+	public float AngularSpeedDeg => _angularSpeed;
+	[SerializeField]
+	private float _minAngle = 0;
+	public float MinAngleDeg => _minAngle;
+	[SerializeField]
+	private float _maxAngle = 135;
+	public float MaxAngleDeg => _maxAngle;
+	[SerializeField]
+	private Axis _axis = Axis.Y;
+	public Axis Axis => _axis;
+	[SerializeField]
+	private bool _movingForward = true;
+	public bool MovingForward {
+		get => _movingForward;
+		set => _movingForward = value;
+	}
+	public float AngleDeg {
+		get {
+			var euler = transform.eulerAngles;
+			switch ( Axis ) {
+			case Axis.X: return	euler.x;
+			case Axis.Y: return	euler.y;
+			case Axis.Z: return	euler.z;
+			default: return	0;
+			}
+		}
+		set {
+			var euler = transform.eulerAngles;
+			switch ( Axis ) {
+			case Axis.X: euler.x = value; break;
+			case Axis.Y: euler.y = value; break;
+			case Axis.Z: euler.z = value; break;
+			}
+
+			transform.eulerAngles = euler;
+		}
+	}
+
+	private void FixedUpdate () {
+		var angleStep = AngularSpeedDeg * Time.fixedDeltaTime;
+		if ( !MovingForward )
+			angleStep = -angleStep;
+
+		var angle = AngleDeg;
+		var minAngle = MinAngleDeg;
+		var maxAngle = MaxAngleDeg;
+		MathHelper.SortMinMax ( ref minAngle, ref maxAngle );
+		bool isOnInnerArc = minAngle <= angle && angle <= maxAngle;
+		float targetAngle;
+		if ( isOnInnerArc )
+			targetAngle = angleStep > 0 ? maxAngle : minAngle;
+		else
+			targetAngle = angleStep > 0 ? minAngle : maxAngle;
+
+		bool metBoundary = MathHelper.StepTowardsAngleDeg ( ref angle, targetAngle, angleStep );
+		if ( metBoundary )
+			MovingForward = !MovingForward;
+
+		AngleDeg = angle;
+	}
+}
