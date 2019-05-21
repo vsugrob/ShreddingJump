1a854c3ef538af6cd553347dbe72bca1628bf125
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-21 19:03:25 +0300

    Add scale shrinking animation.

--- Assets/Code/ObjectRemover.cs
+++ Assets/Code/ObjectRemover.cs
@@ -4,20 +4,33 @@ public class ObjectRemover : MonoBehaviour {
 	[SerializeField]
 	private float _animationDuration = 1;
 	public float AnimationDuration => _animationDuration;
+	[SerializeField]
+	private bool _fadeAlpha = false;
+	public bool FadeAlpha {
+		get => _fadeAlpha;
+		private set => _fadeAlpha = value;
+	}
+	[SerializeField]
+	private bool _shrinkScale = true;
+	public bool ShrinkScale {
+		get => _shrinkScale;
+		private set => _shrinkScale = value;
+	}
 	private float startTime = float.NaN;
 	public bool Started => !float.IsNaN ( startTime );
 	public bool Finished { get; private set; }
 	private Renderer [] renderers;
 	private float [] initialAlphas;
+	private Vector3 initialScale;
 
 	public void StartRemoval () {
 		startTime = Time.time;
-		renderers = GetComponentsInChildren <Renderer> ();
-		initialAlphas = new float [renderers.Length];
-		for ( int i = 0 ; i < renderers.Length ; i++ ) {
-			var renderer = renderers [i];
-			initialAlphas [i] = renderer.sharedMaterial.color.a;
-		}
+		renderers = GetComponentsInChildren<Renderer> ();
+		if ( FadeAlpha )
+			CaptureInitialAlpha ();
+
+		if ( ShrinkScale )
+			initialScale = transform.localScale;
 	}
 
 	private void Update () {
@@ -29,7 +42,19 @@ public class ObjectRemover : MonoBehaviour {
 		if ( !Finished && t == 1 )
 			Finished = true;
 
-		SetAlphas ( t );
+		if ( FadeAlpha )
+			SetAlphas ( t );
+
+		if ( ShrinkScale )
+			SetScale ( t );
+	}
+
+	private void CaptureInitialAlpha () {
+		initialAlphas = new float [renderers.Length];
+		for ( int i = 0 ; i < renderers.Length ; i++ ) {
+			var renderer = renderers [i];
+			initialAlphas [i] = renderer.sharedMaterial.color.a;
+		}
 	}
 
 	private void SetAlphas ( float t ) {
@@ -43,8 +68,14 @@ public class ObjectRemover : MonoBehaviour {
 		}
 	}
 
-	public static bool StartRemoval ( Transform transform ) {
+	private void SetScale ( float t ) {
+		transform.localScale = Vector3.Lerp ( initialScale, Vector3.zero, t );
+	}
+
+	public static bool StartRemoval ( Transform transform, bool fadeAlpha = false, bool shrinkScale = true ) {
 		var remover = transform.gameObject.AddComponent <ObjectRemover> ();
+		remover.FadeAlpha = fadeAlpha;
+		remover.ShrinkScale = shrinkScale;
 		remover.StartRemoval ();
 		return	true;
 	}
