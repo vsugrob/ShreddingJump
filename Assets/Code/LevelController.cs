f366610c0923afb11fe6d84d785f4e357b603546
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-26 19:31:01 +0300

    Wire LevelController to LevelGenerator.

--- Assets/Code/LevelController.cs
+++ Assets/Code/LevelController.cs
@@ -1,14 +1,49 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 public class LevelController : MonoBehaviour {
 	public BouncingBallCharacter Character { get; private set; }
+	[SerializeField]
+	private Transform _floorsContainer = null;
+	public Transform FloorsContainer => _floorsContainer;
 
 	private void Start () {
 		Character = FindObjectOfType <BouncingBallCharacter> ();
 		Character.KillerObstacleHit += Character_KillerObstacleHit;
+		GenerateLevel ();
 	}
 
 	private void Character_KillerObstacleHit ( BouncingBallCharacter character, KillerObstacle obstacle ) {
-		Time.timeScale = 0;
+		//Time.timeScale = 0;
+	}
+
+	private void GenerateLevel () {
+		var generator = GetComponent <LevelGenerator> ();
+		if ( generator == null )
+			return;
+
+		DestroyChildren ( FloorsContainer );
+		var dummyFloorGo = CreateDummyFloor ();
+		var generatorEn = generator
+			.Generate ( dummyFloorGo )
+			.Take ( 10 );
+		foreach ( var floor in generatorEn ) {}
+	}
+	// TODO: move to HierarchyHelper or smth alike.
+	private static void DestroyChildren ( Transform rootTf ) {
+		var count = rootTf.childCount;
+		for ( int i = 0 ; i < count ; i++ ) {
+			var childTf = rootTf.GetChild ( i );
+			Destroy ( childTf.gameObject );
+		}
+	}
+
+	private GameObject CreateDummyFloor () {
+		var rooftopY = Character.transform.position.y + Character.JumpHeight / 2;
+		var floorGo = new GameObject ( "DummyFloor" );
+		var floorTf = floorGo.transform;
+		floorTf.position = Vector3.up * rooftopY;
+		floorTf.parent = FloorsContainer;
+		return	floorGo;
 	}
 }
