201b9f2311340fed9a88b3deb46fc0c10bf2997c
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-26 16:45:43 +0300

    Add prefab database, suitable for upcoming level generator.

---
+++ Assets/Code/GameSettings/PrefabDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PrefabDatabase : ScriptableObject {
+	[SerializeField]
+	private GameObject _character;
+	public GameObject Character {
+		get => _character;
+		set => _character = value;
+	}
+	[SerializeField]
+	private List <GameObject> _platforms = new List <GameObject> ();
+	public List <GameObject> Platforms {
+		get => _platforms;
+		set => _platforms = value;
+	}
+	[SerializeField]
+	private List <GameObject> _walls = new List <GameObject> ();
+	public List <GameObject> Walls {
+		get => _walls;
+		set => _walls = value;
+	}
+	[SerializeField]
+	private List <GameObject> _platformObstacles = new List <GameObject> ();
+	public List <GameObject> PlatformObstacles {
+		get => _platformObstacles;
+		set => _platformObstacles = value;
+	}
+}
