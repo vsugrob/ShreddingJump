4c5d56ef8199ed90856b1235f188acf4b5251964
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-28 19:56:20 +0300

    Start implementing FragmentedCircle<TElement, TLimit>.

---
+++ Assets/Code/Math/FragmentedCircle.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace System.Collections.Generic {
+	[DebuggerDisplay ( "Count = {Count}" )]
+	public class FragmentedCircle <TElement, TLimit>
+		where TLimit : IComparable <TLimit>
+	{
+		private List <CircleFragment <TElement, TLimit>> fragments = new List <CircleFragment <TElement, TLimit>> ();
+		public int Count => fragments.Count;
+
+		public void Add ( TElement element, Range <TLimit> range ) {
+			range.Order ();
+			int index = CountFragmentsLessThanOrEqual ( range.Start );
+			fragments.Insert ( index, new CircleFragment <TElement, TLimit> ( element, range ) );
+		}
+
+		private int CountFragmentsLessThanOrEqual ( TLimit margin ) {
+			int count = 0;
+			for ( int i = 0 ; i < fragments.Count ; i++ ) {
+				if ( fragments [i].Range <= margin )
+					count++;
+			}
+
+			return	count;
+		}
+	}
+}
