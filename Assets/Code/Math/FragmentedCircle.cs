26a92dcc7bc98664bd1855527fa6fb3b9f6aa693
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-29 19:44:36 +0300

    Add min and max limits for FragmentedCircle.
    These can be set to 0 and 360, or, say 0 and Math.Pi.

--- Assets/Code/Math/FragmentedCircle.cs
+++ Assets/Code/Math/FragmentedCircle.cs
@@ -7,6 +7,22 @@ namespace System.Collections.Generic {
 	{
 		private List <CircleFragment <TElement, TLimit>> fragments = new List <CircleFragment <TElement, TLimit>> ();
 		public int Count => fragments.Count;
+		public TLimit MinLimit { get; private set; }
+		public TLimit MaxLimit { get; private set; }
+
+		public FragmentedCircle ( TLimit minLimit, TLimit maxLimit ) {
+			if ( ReferenceEquals ( minLimit, null ) )
+				throw new ArgumentNullException ( nameof ( minLimit ) );
+
+			if ( ReferenceEquals ( maxLimit, null ) )
+				throw new ArgumentNullException ( nameof ( maxLimit ) );
+
+			if ( minLimit.CompareTo ( maxLimit ) >= 0 )
+				throw new ArgumentException ( "maxLimit limit must be greater than minLimit.", nameof ( maxLimit ) );
+
+			this.MinLimit = minLimit;
+			this.MaxLimit = maxLimit;
+		}
 
 		public void Add ( TElement element, TLimit start, TLimit end ) {
 			Add ( element, Range.Create ( start, end ) );
@@ -20,12 +36,22 @@ namespace System.Collections.Generic {
 
 		private int CountFragmentsLessThanOrEqual ( TLimit margin ) {
 			int count = 0;
+			// TODO: optimize for orderd list of fragments. It MUST be better than O(n).
 			for ( int i = 0 ; i < fragments.Count ; i++ ) {
 				if ( fragments [i].Range <= margin )
 					count++;
 			}
 
 			return	count;
+		}
+
+		public Range <TLimit> FindEmptyRange () {
+			var start = MinLimit;
+			var end = MinLimit;
+			for ( int i = 0 ; i < fragments.Count ; i++ ) {
+				var fragment = fragments [i];
+				// TODO: implement.
+			}
 		}
 	}
 }
