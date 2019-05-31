ac644907dba4697bd33c6c9c9fe7daab237c2ef0
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-05-31 20:06:01 +0300

    Add simple unit test for FragmentedCircle.

---
+++ Assets/Code/Math/Fragmented/Editor/FragmentedCircleTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+
+namespace Tests {
+    public class FragmentedCircleTests {
+		[Test]
+		public void FragmentedCircle_AddPositiveNonTrespassingRange () {
+			AssertAddPositiveNonOverlappingRange ( 0, 45 );
+			AssertAddPositiveNonOverlappingRange ( 5, 6 );
+			AssertAddPositiveNonOverlappingRange ( 0, 360 );
+			AssertAddPositiveNonOverlappingRange ( 270, 360 );
+			AssertAddPositiveNonOverlappingRange ( 359, 360 );
+		}
+
+		private void AssertAddPositiveNonOverlappingRange ( float start, float end ) {
+			var circle = FragmentedCircle.CreateDegrees <string> ();
+			const string E0 = "r0";
+			circle.Add ( E0, start, end );
+			Assert.AreEqual ( 1, circle.Count );
+			var f0 = circle [0];
+			Assert.AreEqual ( E0, f0.Element );
+			var r0 = f0.Range;
+			Assert.AreEqual ( start, r0.Start );
+			Assert.AreEqual ( end, r0.End );
+		}
+    }
+}
