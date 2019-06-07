ea906aa21172738dc7b615f1c9d2e8bde7c6dd07
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-06-08 00:28:32 +0300

    Implement Range.SubtractOrdered() and unit tests for it.

--- Assets/Code/Math/Range/Range.cs
+++ Assets/Code/Math/Range/Range.cs
@@ -19,5 +19,48 @@
 		{
 			return	new Range <T> ( point );
 		}
+
+		public static void SubtractOrdered <T> ( Range <T> a, Range <T> b, out Range <T>? r1, out Range <T>? r2 )
+			where T : IComparable <T>
+		{
+			r2 = null;
+			var asbsSign = a.Start.CompareTo ( b.Start );
+			if ( asbsSign == 0 ) {								// a.s == b.s
+				var aebeSign = a.End.CompareTo ( b.End );
+				if ( aebeSign <= 0 )							// a.e <= b.e
+					r1 = null;									// { a.s==b.s, a.e, b.e }
+				else											// a.e > b.e
+					r1 = Create ( b.End, a.End );				// { a.s==b.s, b.e, a.e }
+			} else if ( asbsSign < 0 ) {						// a.s < b.s
+				var aebsSign = a.End.CompareTo ( b.Start );
+				if ( aebsSign <= 0 )							// a.e <= b.s
+					r1 = a;										// { a.s, a.e, b.s, b.e }
+				else {											// a.e > b.s
+					var aebeSign = a.End.CompareTo ( b.End );
+					if ( aebeSign <= 0 )						// a.e <= b.e
+						r1 = Create ( a.Start, b.Start );		// { a.s, b.s, a.e, b.e }
+					else {										// a.e > b.e
+						r1 = Create ( a.Start, b.Start );		// { a.s, b.s, b.e, a.e }
+						r2 = Create ( b.End, a.End );
+					}
+				}
+			} else {											// a.s > b.s
+				var asbeSign = a.Start.CompareTo ( b.End );
+				if ( asbeSign == 0 ) {							// a.s == b.e
+					if ( a.IsPoint )							// a.s == a.e
+						r1 = null;								// { b.s, b.e==a.s==a.e }
+					else
+						r1 = a;									// { b.s, b.e, a.s, a.e }
+				} else if ( asbeSign > 0 )						// a.s > b.e
+					r1 = a;										// { b.s, b.e, a.s, a.e }
+				else {											// a.s < b.e
+					var aebeSign = a.End.CompareTo ( b.End );
+					if ( aebeSign <= 0 )						// a.e <= b.e
+						r1 = null;								// { b.s, a.s, a.e, b.e }
+					else										// a.e > b.e
+						r1 = Create ( b.End, a.End );			// { b.s, a.s, b.e, a.e }
+				}
+			}
+		}
 	}
 }
