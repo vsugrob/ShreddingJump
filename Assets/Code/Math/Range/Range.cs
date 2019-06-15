e2051bf48f91a02c0574939d6f8cff4405c000d5
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-06-16 00:22:47 +0300

    Implement Range.IntersectOrdered() method and unit-tests for it.

--- Assets/Code/Math/Range/Range.cs
+++ Assets/Code/Math/Range/Range.cs
@@ -20,6 +20,42 @@
 			return	new Range <T> ( point );
 		}
 
+		public static void IntersectOrdered <T> ( Range <T> a, Range <T> b, out Range <T>? r )
+			where T : IComparable <T>
+		{
+			var asbsSign = a.Start.CompareTo ( b.Start );
+			if ( asbsSign <= 0 )
+				IntersectOrderedAStartLTOrEqBStart ( a, b, asbsSign, out r );
+			else
+				IntersectOrderedAStartLTOrEqBStart ( b, a, -asbsSign, out r );
+		}
+
+		private static void IntersectOrderedAStartLTOrEqBStart <T> ( Range <T> a, Range <T> b, int asbsSign, out Range <T>? r )
+			where T : IComparable <T>
+		{
+			if ( asbsSign == 0 ) {								// a.s == b.s
+				var aebeSign = a.End.CompareTo ( b.End );
+				if ( aebeSign <= 0 )							// a.e <= b.e
+					r = a;										// { a.s==b.s, a.e, b.e }
+				else											// a.e > b.e
+					r = Create ( a.Start, b.End );				// { a.s==b.s, b.e, a.e }
+			} else if ( asbsSign < 0 ) {						// a.s < b.s
+				var aebsSign = a.End.CompareTo ( b.Start );
+				if ( aebsSign == 0 )							// a.e == b.s
+					r = Create ( a.End, a.End );				// { a.s, a.e==b.s, b.e }
+				else if ( aebsSign < 0 )						// a.e < b.s
+					r = null;									// { a.s, a.e, b.s, b.e }
+				else {											// a.e > b.s
+					var aebeSign = a.End.CompareTo ( b.End );
+					if ( aebeSign <= 0 )						// a.e <= b.e
+						r = Create ( b.Start, a.End );			// { a.s, b.s, a.e, b.e }
+					else										// a.e > b.e
+						r = b;									// { a.s, b.s, b.e, a.e }
+				}
+			} else												// a.s > b.s
+				throw new InvalidOperationException ( "This method expects that a.Start <= b.Start." );
+		}
+
 		public static void SubtractOrdered <T> ( Range <T> a, Range <T> b, out Range <T>? r1, out Range <T>? r2 )
 			where T : IComparable <T>
 		{
