c6db429ab55f89e11f98113467bebd25a65ecdf6
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-06-15 23:47:06 +0300

    Extract circular range coercion into CircleMath.

---
+++ Assets/Code/Math/CircleMath.cs
@@ -0,0 +1,50 @@
+﻿namespace System {
+	public static class CircleMath {
+		public static void CoerceRange (
+			Range <float> range, float pi2,
+			out Range <float> range1, out Range <float>? range2
+		) {
+			range2 = null;
+			var start = range.Start;
+			var end = range.End;
+			var diff = end - start;
+			if ( Math.Abs ( diff ) >= pi2 ) {
+				range1 = Range.Create ( 0, pi2 );
+				return;
+			} else if ( diff == 0 ) {
+				range1 = new Range <float> ( CoerceAngle ( start, pi2 ) );
+				return;
+			}
+
+			MathHelper.SortMinMax ( ref start, ref end );
+			var baseAngle = FindClosestCircleBase ( start, pi2 );
+			start -= baseAngle;
+			end -= baseAngle;
+			if ( end <= pi2 ) {
+				range1 = Range.Create ( start, end );
+			} else {
+				range1 = Range.Create ( 0, end - pi2 );
+				range2 = Range.Create ( start, pi2 );
+			}
+		}
+
+		public static float CoerceAngle ( float angle, float pi2 ) {
+			angle = angle % pi2;
+			if ( angle < 0 )
+				angle += pi2;
+
+			return	angle;
+		}
+
+		public static float FindClosestCircleBase ( float angle, float pi2 ) {
+			var divInt = ( int ) ( angle / pi2 );
+			var b = divInt * pi2;
+			if ( b > angle ) {
+				// It's possible when angle is negative. Calculate base value, that is less than angle.
+				b -= pi2;
+			}
+
+			return	b;
+		}
+	}
+}
--- Assets/Code/Math/Fragmented/FragmentedCircleGeneric.cs
+++ Assets/Code/Math/Fragmented/FragmentedCircleGeneric.cs
@@ -5,7 +5,7 @@
 		{}
 
 		public override void Add ( TElement element, Range <float> range ) {
-			CoerceRange ( range, out var range1, out var range2 );
+			CircleMath.CoerceRange ( range, MaxLimit, out var range1, out var range2 );
 			AddOrdered ( element, range1 );
 			if ( range2.HasValue )
 				AddOrdered ( element, range2.Value );
@@ -34,50 +34,6 @@
 			Add ( element, arcEnds );
 		}
 
-		private void CoerceRange ( Range <float> range, out Range <float> range1, out Range <float>? range2 ) {
-			range2 = null;
-			var start = range.Start;
-			var end = range.End;
-			var diff = end - start;
-			if ( Math.Abs ( diff ) >= MaxLimit ) {
-				range1 = Range.Create ( 0, MaxLimit );
-				return;
-			} else if ( diff == 0 ) {
-				range1 = new Range <float> ( CoerceAngle ( start ) );
-				return;
-			}
-
-			MathHelper.SortMinMax ( ref start, ref end );
-			var baseAngle = FindClosestCircleBase ( start );
-			start -= baseAngle;
-			end -= baseAngle;
-			if ( end <= MaxLimit ) {
-				range1 = Range.Create ( start, end );
-			} else {
-				range1 = Range.Create ( 0, end - MaxLimit );
-				range2 = Range.Create ( start, MaxLimit );
-			}
-		}
-
-		private float CoerceAngle ( float angle ) {
-			angle = angle % MaxLimit;
-			if ( angle < 0 )
-				angle += MaxLimit;
-
-			return	angle;
-		}
-
-		private float FindClosestCircleBase ( float angle ) {
-			var divInt = ( int ) ( angle / MaxLimit );
-			var b = divInt * MaxLimit;
-			if ( b > angle ) {
-				// It's possible when angle is negative. Calculate base value, that is less than angle.
-				b -= MaxLimit;
-			}
-
-			return	b;
-		}
-
 		public void Shift ( float offset ) {
 			var fragments = new LineFragment <TElement, float> [fragmentsByStart.Count];
 			fragmentsByStart.Values.CopyTo ( fragments, 0 );
@@ -90,14 +46,14 @@
 
 		public override int SeekFragmentBoundary ( float start, int dir, out float boundary ) {
 			if ( start != MaxLimit )
-				start = CoerceAngle ( start );
+				start = CircleMath.CoerceAngle ( start, MaxLimit );
 
 			return	base.SeekFragmentBoundary ( start, dir, out boundary );
 		}
 
 		public override int SeekClosestFragmentIndex ( float point, int dir ) {
 			if ( point != MaxLimit )
-				point = CoerceAngle ( point );
+				point = CircleMath.CoerceAngle ( point, MaxLimit );
 
 			int index = base.SeekClosestFragmentIndex ( point, dir );
 			if ( index >= 0 )
