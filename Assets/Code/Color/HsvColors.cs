20352886cff0014aa8e8c6aa0608c8af24b35111
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-07-02 19:30:36 +0300

    Add common color factories - HsvColors.
    Colors in there are same as in UnityEngine.Color.

---
+++ Assets/Code/Color/HsvColors.cs
@@ -0,0 +1,12 @@
+﻿public static class HsvColors {
+	public static readonly HsvColor Red = new HsvColor ( 0, 1, 1 );
+	public static readonly HsvColor Green = new HsvColor ( 0.3333333f, 1, 1 );
+	public static readonly HsvColor Blue = new HsvColor ( 0.6666667f, 1, 1 );
+	public static readonly HsvColor White = new HsvColor ( 0, 0, 1 );
+	public static readonly HsvColor Black = new HsvColor ( 0, 0, 0 );
+	public static readonly HsvColor Yellow = new HsvColor ( 0.1533865f, 0.9843137f, 1 );
+	public static readonly HsvColor Cyan = new HsvColor ( 0.5f, 1, 1 );
+	public static readonly HsvColor Magenta = new HsvColor ( 0.8333333f, 1, 1 );
+	public static readonly HsvColor Gray = new HsvColor ( 0, 0, 0.5f );
+	public static readonly HsvColor Grey = new HsvColor ( 0, 0, 0.5f );
+}
