1f2401adf2a420767a6d930b857a114d91182f54
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-07-05 11:46:26 +0300

    Print table of distance between all generated colors.

--- Assets/Code/Color/Editor/HsvPaletteGeneratorTests.cs
+++ Assets/Code/Color/Editor/HsvPaletteGeneratorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -29,12 +30,39 @@ namespace Tests {
 			}
 
 			using ( var sw = new System.IO.StreamWriter ( @"c:\Temp\palette.html" ) ) {
-				for ( int i = 0 ; i < colors.Count ; i++ ) {
-					var hsv = colors [i];
-					var rgb = hsv.ToRgb ();
-					var rgbHex = ColorUtility.ToHtmlStringRGB ( rgb );
-					sw.WriteLine ( $"<div style=\"width : 500px; height : 20px; background-color : #{rgbHex};\">{hsv}</div>" );
+				sw.WriteLine ( "<table>" );
+				for ( int y = 0 ; y < colors.Count ; y++ ) {
+					var hsv1 = colors [y];
+					var rgb1Hex = ColorUtility.ToHtmlStringRGB ( hsv1.ToRgb () );
+					// Write header row.
+					if ( y == 0 ) {
+						sw.Write ( "<tr><th>&nbsp;</th>" );
+						for ( int x = 0 ; x < colors.Count ; x++ ) {
+							var hsv2 = colors [x];
+							var rgb2Hex = ColorUtility.ToHtmlStringRGB ( hsv2.ToRgb () );
+							sw.Write ( $"<th style=\"background-color : #{rgb2Hex};\">#{rgb2Hex}</th>" );
+						}
+
+						sw.WriteLine ( "</tr>" );
+						continue;
+					}
+
+					sw.WriteLine ( "<tr>" );
+					for ( int x = 0 ; x < colors.Count ; x++ ) {
+						var hsv2 = colors [x];
+						// Write header cell.
+						if ( x == 0 )
+							sw.Write ( $"<th style=\"background-color : #{rgb1Hex};\">#{rgb1Hex}</th>" );
+
+						var d = HsvColor.DistanceInColorCone ( hsv1, hsv2 );
+						string style = d < MinDistance && x != y ? "background-color : red;" : "";
+						sw.Write ( $"<td style=\"{style}\">{d:0.##}</td>" );
+					}
+
+					sw.WriteLine ( "</tr>" );
 				}
+
+				sw.WriteLine ( "</table>" );
 			}
 		}
 	}
