f905fdec253ef76bcac6dddd508bc437562b83d6
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-07-12 16:10:13 +0300

    Add colorizer for characters' trail.

---
+++ Assets/Code/Color/TrailRendererColorizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent ( typeof ( TrailRendererColorizer ) )]
+public class TrailRendererColorizer : Colorizer {
+    private TrailRenderer _renderer;
+	public TrailRenderer Renderer => _renderer ?? ( _renderer = GetComponent <TrailRenderer> () );
+
+	public override bool SetColor ( Color color, MaterialSubstitutionCache cache ) {
+		var renderer = Renderer;
+		if ( renderer == null )
+			return	false;
+
+		renderer.startColor = color;
+		var ec = color;
+		ec.a = renderer.endColor.a;
+		renderer.endColor = ec;
+		return	true;
+	}
+}
