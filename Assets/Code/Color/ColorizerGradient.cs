909da212287131b7fce4f60801579b369744b355
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-07-15 18:41:01 +0300

    Add colorizer abstraction for multiple colors.

---
+++ Assets/Code/Color/ColorizerGradient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ColorizerGradient : Colorizer {
+	[SerializeField]
+	private List <GradientStop> _gradientStops = new List <GradientStop> ();
+	public List <GradientStop> GradientStops {
+		get => _gradientStops;
+		set => _gradientStops = value;
+	}
+	private Gradient _gradient;
+	public Gradient Gradient => _gradient ?? InitGradient ();
+
+	private Gradient InitGradient () {
+		var stops = GradientStops;
+		int count = stops.Count;
+		var colorKeys = new GradientColorKey [count];
+		for ( int i = 0 ; i < count ; i++ ) {
+			var stop = stops [i];
+			colorKeys [i].time = stop.Position;
+		}
+
+		_gradient = new Gradient {
+			colorKeys = colorKeys
+		};
+		return	_gradient;
+	}
+
+	public override bool SetColor ( IReadOnlyDictionary <ColorRole, HsvColor> palette, MaterialSubstitutionCache cache ) {
+		var stops = GradientStops;
+		int count = stops.Count;
+		var colorKeys = Gradient.colorKeys;
+		for ( int i = 0 ; i < count ; i++ ) {
+			var stop = stops [i];
+			if ( !palette.TryGetValue ( stop.Role, out var hsvColor ) )
+				continue;
+
+			colorKeys [i].color = ( Color ) hsvColor;
+		}
+
+		return	true;
+	}
+
+	[Serializable]
+	public struct GradientStop {
+		public float Position;
+		public ColorRole Role;
+	}
+}
