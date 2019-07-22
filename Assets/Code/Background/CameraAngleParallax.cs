87a587391cd879df07cd33c771a73fb39f740d08
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-07-22 19:19:10 +0300

    Make city background rotate according to camera view angle.

---
+++ Assets/Code/Background/CameraAngleParallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraAngleParallax : MonoBehaviour {
+	[SerializeField]
+	private float _unitsPerFullRound = 10;
+	public float UnitsPerFullRound {
+		get => _unitsPerFullRound;
+		set => _unitsPerFullRound = value;
+	}
+	private float initialX;
+
+	private void Start () {
+		initialX = transform.localPosition.x;
+	}
+
+	private void LateUpdate () {
+		var cam = Camera.main;
+		if ( cam == null ) return;
+		var angle = MathHelper.ToNormAngleDeg ( cam.transform.eulerAngles.y );
+		float t = angle / 360;
+		var pos = transform.localPosition;
+		pos.x = initialX + t * UnitsPerFullRound;
+		transform.localPosition = pos;
+	}
+}
