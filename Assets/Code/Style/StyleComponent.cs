6ab0e70f9f08c670de1378f4983add642e7539af
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-08-08 19:20:10 +0300

    Add StyleComponent describing visual appearance qualities of prefabs.

---
+++ Assets/Code/Style/StyleComponent.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StyleComponent : MonoBehaviour {
+	[SerializeField]
+	private List <WeightedTag> _tags = new List <WeightedTag> ();
+	public List <WeightedTag> Tags => _tags;
+}
