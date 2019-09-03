f81c70c634bd86a882f97fa7accebfdaf3a9a496
Vitaliy E. Sugrobov <vsugrob@hotmail.com>
2019-09-03 19:16:23 +0300

    Put StyleDistance classes into separate files.

--- Assets/Code/Style/StyleComponentExt.cs
+++ Assets/Code/Style/StyleComponentExt.cs
@@ -9,27 +9,3 @@ public static class StyleComponentExt {
 		return	components.Select ( c => StyleDistance.Create ( c, StyleComponent.Distance ( reference, c ) ) );
 	}
 }
-
-public struct StyleDistance <TComponent>
-	where TComponent : Component
-{
-	public TComponent Component;
-	public float Distance;
-
-	public StyleDistance ( TComponent component, float distance ) {
-		this.Component = component;
-		this.Distance = distance;
-	}
-
-	public override string ToString () {
-		return	$"({Component}, Distance: {Distance})";
-	}
-}
-
-public static class StyleDistance {
-	public static StyleDistance <TComponent> Create <TComponent> ( TComponent component, float distance )
-		where TComponent : Component
-	{
-		return	new StyleDistance <TComponent> ( component, distance );
-	}
-}
---
+++ Assets/Code/Style/StyleDistanceGeneric.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public struct StyleDistance <TComponent>
+	where TComponent : Component
+{
+	public TComponent Component;
+	public float Distance;
+
+	public StyleDistance ( TComponent component, float distance ) {
+		this.Component = component;
+		this.Distance = distance;
+	}
+
+	public override string ToString () {
+		return	$"({Component}, Distance: {Distance})";
+	}
+}
+
